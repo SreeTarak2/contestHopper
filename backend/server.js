e02de33d99e3d964ce603b39be2d263b66b87635
SreeTarak2 <jstvamsikrishna2@gmail.com>
2025-07-17 14:55:18 +0530

    insertData is moved out

--- backend/server.js
+++ backend/server.js
@@ -61,5 +61,5 @@ app.get("/search", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`🚀 Server running at ${PORT}`);
 });
