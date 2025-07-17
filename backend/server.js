b0e004e7588cb91203c9d1ab63cc43b68b99126c
SreeTarak2 <jstvamsikrishna2@gmail.com>
2025-07-17 15:45:14 +0530

    one link from cors removed

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin:["https://contesthopper.onrender.com","https://contesthopper.pages.dev",]
+  origin:["https://contesthopper.pages.dev",]
 }));
 app.use(express.json());
 
