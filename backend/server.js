9f8026a42a2dbb5cac2603b123433543209b5cea
SreeTarak2 <122889552+SreeTarak2@users.noreply.github.com>
2025-09-18 07:46:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,9 @@ const corsOptions = {
   origin: [
     "https://contesthopper.live",
     "https://contesthopper.pages.dev",
-    "http://127.0.0.1:5500",
+    // "http://127.0.0.1:5500",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
   ],
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type"],
